5cd4dc5dbe3a7d592f2a1cf646e223ef9dfa3ec0
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-04-02 18:46:10 +0200

    worked on text.js: 'Select a Cell Phone functionality' - 'Check if error is thrown ("The Model Name must be one of the 5 available Models!")'

--- test.js
+++ test.js
@@ -412,6 +412,9 @@ describe('Purchase', () => {
             it('should throw error if parameter is an Empty String', function(){
                 expect(() => purchase.selectCellPhone(String(''))).to.throw('The Model Name must be one of the 5 available Models!');
             });
+            it('should throw error if parameter is NOT A PHONE NAME', function(){
+                expect(() => purchase.selectCellPhone(String('NOT A PHONE NAME'))).to.throw('The Model Name must be one of the 5 available Models!');
+            });
 
         });
 
